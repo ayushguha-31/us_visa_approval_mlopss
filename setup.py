bb73240d910d19360b11b4cf3c3e48516e442e71
ayushguha-31 <ayushguha31.com>
2024-10-10 16:50:47 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Ayush",
+    author_email="ayushguha31@gmail.com",
+    packages=find_packages()
+)
